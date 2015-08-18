8648cfa670b02715817de0ba1de8b3bffb4e4464
James Quinn <jquinn@roomkey.com>
2015-08-17 22:36:47 -0400

    move create table to node start script; release connection on table create completion

--- bin/listen.js
+++ bin/listen.js
@@ -2,8 +2,18 @@ var _       = require('lodash')
   , config  = require('../lib/config')
   , log     = require('../lib/log')(module)
   , queue   = require('../lib/queue')
+  , db      = require('../lib/db')
   ;
 
+//make sure the table is there
+db.createTable( )
+  .then(function(){
+    // log.info('table created');
+  })
+  .catch(function(err){
+    log.error('',err);
+  })
+;
 
 log.info('starting SQS long-polling');
 
